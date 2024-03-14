f92b6e45cb9e39ac61fc9ea42227cff3bd9c6bbc
Gahramanoval301 <lemangahramanoval@gmail.com>
2024-03-14 23:10:03 +0300

    all questions and answers file (data file)

---
+++ QuizApp/data.js
@@ -0,0 +1,37 @@
+export const questions =[ {
+    question: "Which is larger animal in the world?",
+    answers: [
+        {text:"Shark", correct: false},
+        {text:"Blue Whale", correct: true},
+        {text:"Elephant", correct: false},
+        {text:"Giraffe", correct: false}
+    ]
+},
+{
+    question: "Which is the smallest continent in the world?",
+    answers: [
+        {text:"Asia", correct: false},
+        {text:"Australia", correct: true},
+        {text:"Arctic", correct: false},
+        {text:"Africa", correct: false}
+    ]
+},
+{
+    question: "Which is the smallest country in the world?",
+    answers: [
+        {text:"Vatican City", correct: true},
+        {text:"Btuhan", correct: false},
+        {text:"Nepal", correct: false},
+        {text:"Shri Lanka", correct: false}
+    ]
+},
+{
+    question: "Which is the largest desert in the world?",
+    answers: [
+        {text:"Kalahari", correct: false},
+        {text:"Gobi", correct: false},
+        {text:"Sahara", correct: false},
+        {text:"Antarctica", correct: true}
+    ]
+},]
+
